642af6fb7229d085e03e922386ac516fa2a4d299
hamzon55 <hamzon.jb@gmail.com>
2024-08-26 14:28:09 +0200

    Initial Commit

---
+++ SwiftUiApp/SwiftUiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiAppApp.swift
+//  SwiftUiApp
+//
+//  Created by Hamza Jerbi on 26/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
